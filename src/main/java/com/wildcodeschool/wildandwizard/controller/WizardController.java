cd43aad1bf434aa77382e91fd73b1af0f1bc0e3b
Michael Blisner <Michael.Blisner@telekom.de>
2020-01-13 17:03:15 +0100

    loesung

--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -3,7 +3,6 @@ package com.wildcodeschool.wildandwizard.controller;
 import com.wildcodeschool.wildandwizard.repository.WizardRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,20 +11,15 @@ import java.sql.Date;
 @Controller
 public class WizardController {
 
-    private WizardRepository repository = new WizardRepository();
+	private WizardRepository repository = new WizardRepository();
 
-    @PostMapping("/wizard/create")
-    public String postWizard(Model model,
-                             @RequestParam String firstName,
-                             @RequestParam String lastName,
-                             @RequestParam Date birthday,
-                             @RequestParam String birthPlace,
-                             @RequestParam(required = false, defaultValue = "") String biography,
-                             @RequestParam(required = false, defaultValue = "false") boolean muggle
-    ) {
-        model.addAttribute("wizard", repository.save(firstName, lastName,
-                birthday, birthPlace, biography, muggle));
+	@PostMapping("/wizard/create")
+	public String postWizard(Model model, @RequestParam String firstName, @RequestParam String lastName,
+			@RequestParam Date birthday, @RequestParam String birthPlace,
+			@RequestParam(required = false, defaultValue = "") String biography,
+			@RequestParam(required = false, defaultValue = "false") boolean muggle) {
+		model.addAttribute("wizard", repository.save(firstName, lastName, birthday, birthPlace, biography, muggle));
 
-        return "wizard_get";
-    }
+		return "wizard_get";
+	}
 }
